920da27485a43abe097faae98cac99c0bd2c0019
ZzHardcandyzZ <125589956+ZzHardcandyzZ@users.noreply.github.com>
2023-02-23 17:31:40 +0200

    Add files via upload

---
+++ Untitled-e34234234234.py
@@ -0,0 +1,14 @@
+username = ["adam" , "ahmed" , "ali" , "omar"]
+password = ["012" , "123" , "0100" , "011"]
+Name= input("Enter your username: ")
+passs= input("Enter your password:" )
+if (Name == username[0] and passs == password[0]):
+    print("welcome Adam")    
+elif (Name == username[0] and passs == password[0]):
+    print("welcome ahmed")    
+elif (Name == username[0] and passs == password[0]):
+    print("welcome ali")    
+elif (Name == username[0] and passs == password[0]):
+    print("welcome omar") 
+else:
+    print("Wrong username or password")
